fc4f363497d5833fd79691668a4c4e3a8215b02e
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-12-02 21:32:11 +0000

    includes succesfully connected to server message and sends 3 char name to server

--- client.js
+++ client.js
@@ -13,6 +13,11 @@ const connect = function() {
     console.log('->', data)
   })
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server!");
+    conn.write('Name: OMG');
+  })
+
   return conn;
 }
 
